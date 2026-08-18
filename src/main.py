a45fab7af801e1ae06799267c662ccce7b962967
lewisacidsann <pokekuri12@gmail.com>
2026-08-18 18:33:58 +0900

    feat:csvに保存できた

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from google import genai
 from google.genai import types
+
 from dotenv import load_dotenv
+
+from pathlib import Path
+
 import os
+import json
+import csv
+
+# =========================
+# 設定
+# =========================
 
 load_dotenv()
 
@@ -9,13 +19,42 @@ client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-with open("receipts/image.jpeg", "rb") as f:
+# =========================
+# レシート画像検索
+# =========================
+
+receipt_dir = Path("receipts")
+
+images = (
+    list(receipt_dir.glob("*.jpg"))
+    + list(receipt_dir.glob("*.jpeg"))
+    + list(receipt_dir.glob("*.png"))
+)
+
+if len(images) == 0:
+    raise FileNotFoundError(
+        "receiptsフォルダに画像がありません"
+    )
+
+image_path = images[0]
+
+print(f"読み込みファイル: {image_path}")
+
+# =========================
+# 画像読み込み
+# =========================
+
+with open(image_path, "rb") as f:
     image_bytes = f.read()
+
+# =========================
+# Geminiへ送信
+# =========================
 
 prompt = """
 このレシートから情報を抽出してください。
 
-以下のJSONのみを出力してください。
+以下のJSONのみ出力してください。
 
 {
   "purchase_date": "",
@@ -23,7 +62,10 @@ prompt = """
   "total_amount": 0
 }
 
-JSON以外出力しないこと。
+ルール:
+- JSON以外出力しない
+- 日付は YYYY-MM-DD
+- 金額は数値のみ
 """
 
 response = client.models.generate_content(
@@ -32,10 +74,51 @@ response = client.models.generate_content(
         prompt,
         types.Part.from_bytes(
             data=image_bytes,
-            mime_type="image/jpeg",
-        ),
+            mime_type="image/jpeg"
+        )
     ],
+    config=types.GenerateContentConfig(
+        response_mime_type="application/json"
+    )
 )
 
+# =========================
+# JSON変換
+# =========================
 
-print(response.text)
+data = json.loads(response.text)
+
+print("取得データ")
+print(json.dumps(data, ensure_ascii=False, indent=2))
+
+# =========================
+# CSV保存
+# =========================
+
+csv_path = "receipts.csv"
+
+file_exists = os.path.exists(csv_path)
+
+with open(
+    csv_path,
+    "a",
+    newline="",
+    encoding="utf-8"
+) as f:
+
+    writer = csv.writer(f)
+
+    if not file_exists:
+        writer.writerow([
+            "購入日",
+            "購入先",
+            "金額"
+        ])
+
+    writer.writerow([
+        data["purchase_date"],
+        data["store"],
+        data["total_amount"]
+    ])
+
+print("CSV保存完了")
